aa14c880f3af5564cadaf860d5f928cc91087e45
Serse12 <antonio.allocca12345@live.it>
2017-02-11 18:36:55 +0100

    Initial Commit
    0.0.1

---
+++ Assets/Attacco.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacco : MonoBehaviour {
+
+    public float CaricamentoAttacco = 0f;
+    public float CaricamentoAttaccoDebole = 5f;
+    public float CaricamentoAttaccoMedio = 10f;
+    public float CaricamentoAttaccoForte = 15f;
+
+
+
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.Space))
+        {
+            CaricamentoAttacco += Time.deltaTime * 5f;
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Spadata();
+        }
+	}
+
+    void Spadata()
+    {
+        if(CaricamentoAttacco >= CaricamentoAttaccoForte)
+        {
+            Debug.Log("Attacco FORTE!");
+        }
+        else if (CaricamentoAttacco >= CaricamentoAttaccoMedio)
+        {
+            Debug.Log("Attacco Medio!");
+        }
+        else
+        {
+            Debug.Log("Attacco debole :( ");
+        }
+
+        CaricamentoAttacco = 0f;
+
+    }
+}
---
+++ Assets/llllllllllllllllllllllllllllll.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class llllllllllllllllllllllllllllll : MonoBehaviour {
+    public int amount = 2;
+    public int cont = 0;
+    public bool enableRotation= false;
+    public bool enableScale = false;
+    public bool enablePosition = false;
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {//ciao
+        if (cont++ == 10) { amount *= -1; cont = 0; }
+        if (enableRotation)gameObject.GetComponent<Renderer>().transform.Rotate(new Vector3(0,0,1), 2);
+        if (enablePosition) gameObject.GetComponent<Renderer>().transform.position=new Vector3(
+            gameObject.GetComponent<Renderer>().transform.position.x+amount,
+            gameObject.GetComponent<Renderer>().transform.position.y,
+            gameObject.GetComponent<Renderer>().transform.position.z
+        );
+        if (enableScale) gameObject.GetComponent<Renderer>().transform.localScale = new Vector3(cont,cont,1);
+    }
+}
